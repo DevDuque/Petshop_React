d8a126327c5cba43171e2e0a35156e5dcfc4ac21
Davih Duque <davihduque8@gmail.com>
2024-03-04 21:39:42 -0300

    Create styles for FormInput and Show result only when submitted

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,3 @@
-// React
 import React, { useState } from 'react';
 
 // Componentes e Utilidades
@@ -7,16 +6,21 @@ import { calculatePrice } from './utils/Calc';
 
 function App() {
   const [result, setResult] = useState({ bestShop: "", totalPrice: 0 });
+  // Estado para controlar se o formulário foi enviado
+  const [formSubmitted, setFormSubmitted] = useState(false); 
 
   const handleFormSubmit = (date: Date, smallDogs: number, bigDogs: number) => {
     const { bestShop, totalPrice } = calculatePrice(date, smallDogs, bigDogs);
     setResult({ bestShop, totalPrice });
+    setFormSubmitted(true); // Definindo o enviado
   };
 
   return (
-    <div className="App">
+    <div className="App" style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
       <InputForm onSubmit={handleFormSubmit} />
-      <h2>Melhor petshop: {result.bestShop} Preço Total: R$ {result.totalPrice.toFixed(2)}</h2>
+      {formSubmitted && ( // Renderize o <h2> apenas se o formulário foi submetido
+        <h2 style={{ textAlign: 'center', color: '#121212', fontSize: '24px', fontWeight: '800' }}>Melhor petshop: {result.bestShop} Preço Total: R$ {result.totalPrice.toFixed(2)}</h2>
+      )}
     </div>
   );
 }
--- web/src/components/InputForm/InputForm.tsx
+++ web/src/components/InputForm/InputForm.tsx
@@ -1,46 +1,50 @@
 import React, { useState } from 'react';
+import './InputForm.css';
 
 const InputForm: React.FC<{ onSubmit: (date: Date, smallDogs: number, bigDogs: number) => void }> = ({ onSubmit }) => {
     const [date, setDate] = useState<string>('');
-    const [smallDogs, setSmallDogs] = useState<number>(0);
-    const [bigDogs, setBigDogs] = useState<number>(0);
+    const [smallDogs, setSmallDogs] = useState<string>(''); // Inicialize com uma string vazia ('')
+    const [bigDogs, setBigDogs] = useState<string>(''); // Inicialize com uma string vazia ('')
 
     const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
+        e.preventDefault();
 
-    // Validar se todos os campos estão preenchidos
-    if (!date || smallDogs <= 0 || bigDogs <= 0) {
-        alert('Por favor, preencha todos os campos corretamente.');
-        return;
+        // Validar se todos os campos estão preenchidos
+        if (!date || !smallDogs || !bigDogs || parseInt(smallDogs) <= 0 || parseInt(bigDogs) <= 0) {
+            alert('Por favor, preencha todos os campos corretamente.');
+            return;
+        }
+
+        // Extrair dia, mês e ano da string da data no formato brasileiro (DD-MM-YYYY)
+        const [day, month, year] = date.split('-').map(Number);
+
+        // Criar a data no formato ISO (YYYY-MM-DD)
+        const isoDate = `${year}-${month.toString().padStart(2, '0')}-${day.toString().padStart(2, '0')}`;
+
+        // Criar a instância de Date a partir da data no formato ISO
+        const parsedDate = new Date(isoDate);
+
+        // Chamar a função onSubmit com os dados
+        onSubmit(parsedDate, parseInt(smallDogs), parseInt(bigDogs));
     }
 
-    // Extrair dia, mês e ano da string da data no formato brasileiro (DD-MM-YYYY)
-    const [day, month, year] = date.split('-').map(Number);
-
-    // Criar a data no formato ISO (YYYY-MM-DD)
-    const isoDate = `${year}-${month.toString().padStart(2, '0')}-${day.toString().padStart(2, '0')}`;
-
-    // Criar a instância de Date a partir da data no formato ISO
-    const parsedDate = new Date(isoDate);
-
-    // Chamar a função onSubmit com os dados
-    onSubmit(parsedDate, smallDogs, bigDogs);
-}
-    
     return (
-        <form onSubmit={handleSubmit}>
-            {/* DateInput */}
-            <input type="date" placeholder="Data" value={date} onChange={(e) => setDate(e.target.value)} />
+        <div className='FormContainer'>
+            <form className='Form' onSubmit={handleSubmit}>
+                <h1> PetShops </h1>
+                {/* DateInput */}
+                <input className='DateInput' type="date" placeholder="Data" value={date} onChange={(e) => setDate(e.target.value)} />
 
-            {/* SmallDogsInput */}
-            <input type="number" placeholder="Quantidade de cães pequenos" value={smallDogs} onChange={(e) => setSmallDogs(parseInt(e.target.value))} />
+                {/* SmallDogsInput */}
+                <input className='DogsInput' type="number" placeholder="Quantidade de cães pequenos" value={smallDogs} onChange={(e) => setSmallDogs(e.target.value)} />
 
-            {/* BigDogsInput */}
-            <input type="number" placeholder="Quantidade de cães grandes" value={bigDogs} onChange={(e) => setBigDogs(parseInt(e.target.value))} />
+                {/* BigDogsInput */}
+                <input className='DogsInput' type="number" placeholder="Quantidade de cães grandes" value={bigDogs} onChange={(e) => setBigDogs(e.target.value)} />
 
-            {/* Button */}
-            <button type="submit">Calcular</button>
-        </form>
+                {/* Button */}
+                <button className='Button' type="submit">Calcular</button>
+            </form>
+        </div>
     )
 };
 
