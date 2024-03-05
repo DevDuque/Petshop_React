a991624d6217d5d5026c14158a5806b1fe2c1050
Davih Duque <davihduque8@gmail.com>
2024-03-05 04:33:38 -0300

    Add new colors to the form

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -6,20 +6,25 @@ import { calculatePrice } from './utils/Calc';
 
 function App() {
   const [result, setResult] = useState({ bestShop: "", totalPrice: 0 });
+
   // Estado para controlar se o formulário foi enviado
   const [formSubmitted, setFormSubmitted] = useState(false); 
 
   const handleFormSubmit = (date: Date, smallDogs: number, bigDogs: number) => {
     const { bestShop, totalPrice } = calculatePrice(date, smallDogs, bigDogs);
     setResult({ bestShop, totalPrice });
-    setFormSubmitted(true); // Definindo o formulario como enviado
+
+    // Definindo o formulario como enviado
+    setFormSubmitted(true);
   };
 
   return (
     <div className="App" style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
       <InputForm onSubmit={handleFormSubmit} />
-      {formSubmitted && ( // Renderizando o <h2> apenas se o formulário foi submetido
-        <h2 style={{ textAlign: 'center', color: '#121212', fontSize: '24px', fontWeight: '800' }}>Melhor petshop: {result.bestShop} Preço Total: R$ {result.totalPrice.toFixed(2)}</h2>
+
+      {/* Renderizando o <h2> apenas se o formulário foi submetido */}
+      {formSubmitted && ( 
+        <h2 style={{ textAlign: 'center', color: '#A7BFDE', fontSize: '24px', fontWeight: '800' }}>Melhor petshop: {result.bestShop} Preço Total: R$ {result.totalPrice.toFixed(2)}</h2>
       )}
     </div>
   );
